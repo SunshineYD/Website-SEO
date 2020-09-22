5afd52596ea99852ee633cf577ca9613908c5702
PC-20200619WOKH\Administrator <harlan.yang@yuanqu-tech.com>
2020-09-22 10:10:47 +0800

    静态页面多语言，图片懒加载

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Website-SEO
 the code of  Website SEO
+
+### 静态页面多语言切换
+
+    home.html、index.html、language.js
+
+### 图片懒加载
+
+    lazyLoading.html
---
+++ js/language.js
@@ -0,0 +1,22 @@
+var langArr = {
+	"cn": {
+		"index":{
+			"index_1": "这是一个测试标题",
+			"index_2": "这是测试内容"
+		},
+		"home": {
+			"home_1": "这是主页面内容",
+			"home_2": "企业微信"
+		}
+	},
+	"en": {
+		"index":{
+			"index_1": "this is a test title",
+			"index_2": "this is test content"
+		},
+		"home": {
+			"home_1": "this is main page content",
+			"home_2": "Business weChat"
+		}
+	}
+}
